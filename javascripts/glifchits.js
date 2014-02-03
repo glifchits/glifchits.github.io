ca147c8772aac6233b0d3d9f1e010e46b44cb708
George Lifchits <george.lifchits@gmail.com>
2014-02-03 00:50:46 -0500

    no contact form; trim the JS and CSS and adding contact styles

--- javascripts/glifchits.js
+++ javascripts/glifchits.js
@@ -2,37 +2,12 @@
  * glifchits.js
  * 
  * George Lifchits
- * 2013
+ * 2014
  *
  * Javascript for my personal website
  */
 
 $(function(){
-    // textarea autoresize functionality
-    $('textarea').autosize();
-
-    // email validation
-    var isValidEmail = function(email) {
-        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    }
-    $('input#email').keyup(function() {
-        var emailInput = $(this).val();
-        if (isValidEmail(emailInput)) 
-        $(this).addClass('valid');
-        else
-        $(this).removeClass('valid');
-    });
-
-    // alert box close
-    $('.close i').click(function() {
-        $('.alert').css('display','none');
-    });
-
-    var pxToFloat = function(px) {
-        return parseFloat(px.substring(0, px.length - 2));
-    }
-
     // smooth scroll
     // sectionHeightOffset = 0.5 * pictureHeight + padding(17px)
     var sectionHeightOffset = ($('div.picture')[0].clientHeight / 2) + 17;
@@ -44,39 +19,5 @@ $(function(){
         return false;
     });
 
-    // submit email
-    $('#contact #send').click(function() {
-        var alertBox = $('.alert');
-        var alertText = $('.alert .message');
-
-        var emailData = {
-            'sender': $('form [name=sender]').val(),
-        'subject': $('form [name=subject]').val(),
-        'message': $('form [name=message]').val()
-        };
-
-        alertBox.css('display', 'block');
-        if (!emailData.sender)
-        alertText.text("Please enter an email address I can reach you at!");
-        else if (!(isValidEmail(emailData.sender)))
-        alertText.text("That doesn't look like a valid email address. Try again!");
-        else if (!emailData.subject && !emailData.message)
-        alertText.text("Hmm, seems that you haven't entered a subject or message. What are you trying to say here?");
-        else {
-            alertText.text("Sending email...");
-            $.ajax({
-                type: 'POST',
-                url: '/mail',
-                data: emailData,
-                success: function(res) {
-                    var msg = (res.success ? "" : "Your message wasn't sent! ") + res.msg;
-                    alertText.text(msg);
-                },
-                error: function() {
-                           alertText.text("An error occurred making the POST request.");
-                       }
-            });
-        };
-    });
 });
 
