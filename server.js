fb0b72a6609ed0b21911682f83ffb8722ae37ea8
George Lifchits <george.lifchits@gmail.com>
2015-06-07 23:17:16 -0700

    react hot loader app structure is here

---
+++ server.js
@@ -0,0 +1,17 @@
+// thanks to
+// https://raw.githubusercontent.com/gaearon/react-hot-boilerplate/v1.0.0/server.js
+
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:3000');
+});
