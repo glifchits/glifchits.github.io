980b7898a0877eee8f25042ebab04ef733001e02
George Lifchits <george.lifchits@gmail.com>
2015-07-22 14:11:12 -0400

    fixed paragraph styling problems

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 class Section extends React.Component {
 
   render() {
-    let { children, ...props } = this.props;
+    const { children, ...props } = this.props;
     return (
       <section {...props}>
         <div className="picture"/>
@@ -22,34 +22,42 @@ export default class Content extends React.Component {
     return (
       <main>
         <Section id="education">
-          I'm a student at Wilfrid Laurier University in Ontario, Canada. I'm in
-          the business and computer science double degree program, with a co-op
-          option. This means that I hope to graduate with two degrees &mdash; a
-          Bachelor of Business Administration and a Bachelor's of Science in
-          Computer Science &mdash; and I'm gaining work experience along the way!
+          <p>
+            I'm a student at Wilfrid Laurier University in Ontario, Canada. I'm in
+            the business and computer science double degree program, with a co-op
+            option. This means that I hope to graduate with two degrees &mdash; a
+            Bachelor of Business Administration and a Bachelor's of Science in
+            Computer Science &mdash; and I'm gaining work experience along the way!
+          </p>
         </Section>
         <Section id="dev-skills">
-          I'm a skilled developer with substantial experience working on the
-          full stack. I'm proficient in Python and am comfortable working with
-          HTML, CSS, Javascript, SQL, C, Java, the command line, and whatever
-          else you'd like to throw at me! In my spare time I like to take free
-          online courses via Coursera and build webapps to get experience
-          working with some new tech skill I haven't used before.
+          <p>
+            I'm a skilled developer with substantial experience working on the
+            full stack. I'm proficient in Python and am comfortable working with
+            HTML, CSS, Javascript, SQL, C, Java, the command line, and whatever
+            else you'd like to throw at me! In my spare time I like to take free
+            online courses via Coursera and build webapps to get experience
+            working with some new tech skill I haven't used before.
+          </p>
         </Section>
         <Section id="biz-skills">
-          I have knowledge of the business world and a firm understanding of
-          business logic. In school, I've completed a new venture project and
-          done business cases. I have learned a ton of Excel, VBA, and
-          understand decision modeling using Crystal Ball and Arena. I've also
-          taken accounting classes, business law, and so forth.
+          <p>
+            I have knowledge of the business world and a firm understanding of
+            business logic. In school, I've completed a new venture project and
+            done business cases. I have learned a ton of Excel, VBA, and
+            understand decision modeling using Crystal Ball and Arena. I've also
+            taken accounting classes, business law, and so forth.
+          </p>
         </Section>
         <Section id="resume">
-          <p>If you like, you can take a look at my resume below.</p>
-          <br/>
-          <a href="george-lifchits-resume.pdf">Click</a>
-          for a PDF version to view in full screen)
+          <p>
+            If you like, you can take a look at my resume below.
+            <br/>
+            <a href="george-lifchits-resume.pdf">Click</a> for a PDF version to
+            view in full screen)
+          </p>
           <a className="resume" href="george-lifchits-resume.pdf">
-            <img src="images/resume.jpg" />
+            <img src="/src/images/resume.jpg" />
           </a>
         </Section>
         <Section id="contact">
