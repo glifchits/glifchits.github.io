73697dd76b5a2097375755bbd8c223335d429a5c
George Lifchits <george.lifchits@gmail.com>
2015-07-27 11:41:06 -0400

    add year to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        George Lifchits
+        George Lifchits, { new Date().getUTCFullYear() }
       </footer>
     );
   }
