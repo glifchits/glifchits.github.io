eaace34cb23e955594eab0f2beaebb1d20d4467d
George Lifchits <george.lifchits@gmail.com>
2015-07-27 13:03:38 -0400

    improve navbar onClick page scrolling
    for the navbar links, now uses onClick instead of a global jQuery
    listener for click scrolling. Also adjusted the hitspace of the links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,30 +4,41 @@ import $ from 'jquery';
 require('./styles/Navbar.less');
 
 
-export default class Navbar extends React.Component {
+class HashLink extends React.Component {
 
-  componentDidMount() {
-    var sectionHeightOffset = ($('div.picture')[0].clientHeight / 2) + 17;
-    $('a[href*=#]').click(function() {
-      var target = $(this.hash);
-      $('html, body').animate({
-        scrollTop: target.offset().top - sectionHeightOffset
-      }, 300);
-      return false;
-    });
+  onClick(evt) {
+    evt.preventDefault();
+    const href = this.props.href;
+    const offset = 71;
+    const target = $(href);
+    $('body').animate({
+      scrollTop: target.offset().top - offset
+    }, 300);
   }
 
   render() {
+    const { href, children, ...props } = this.props;
     return (
-      <div className="navbar">
+      <a href={href} onClick={this.onClick.bind(this)}>{children}</a>
+    );
+  }
+
+}
+
+
+export default class Navbar extends React.Component {
+
+  render() {
+    return (
+      <nav className="navbar">
         <ul>
-          <li><a href="#education">Education</a></li>
-          <li><a href="#dev-skills">Developer</a></li>
-          <li><a href="#biz-skills">Business-person</a></li>
-          <li><a href="#resume">Resume</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><HashLink href="#education">Education</HashLink></li>
+          <li><HashLink href="#dev-skills">Developer</HashLink></li>
+          <li><HashLink href="#biz-skills">Business-person</HashLink></li>
+          <li><HashLink href="#resume">Resume</HashLink></li>
+          <li><HashLink href="#contact">Contact</HashLink></li>
         </ul>
-      </div>
+      </nav>
     );
   }
 
